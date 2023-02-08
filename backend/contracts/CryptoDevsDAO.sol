96e9505a7bfa7ca9179a303942db7ea29578f8b3
raizo07 <agadaraizo@gmail.com>
2023-02-08 15:59:21 +0100

    updated proposal

--- backend/contracts/CryptoDevsDAO.sol
+++ backend/contracts/CryptoDevsDAO.sol
@@ -17,7 +17,7 @@ interface ICryptoDevsNFT {
     function tokenOfOwnerByIndex(address owner, uint256 index)
     external
     view
-    returns(uint256;)
+    returns(uint256);
 }
 
 
@@ -25,5 +25,40 @@ interface ICryptoDevsNFT {
 
 
 contract CrptoDevsDao is Ownable {
+struct Proposal {
+
+    uint256 nftTokenId;
+
+    uint256 deadline;
+
+    uint256 yayVotes;
+
+    uint256 nayVotes;
+
+    bool executed;
+
+    mapping(uint256 => bool) voters;
+}
+
+mapping(uint256 => Proposal) public proposals;
+
+    uint256 public numProposals;
+
+    IFakeNFTMarketplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+        nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
+        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+ modifier nftHolderOnly() {
+            require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+            _;
+        }
+
+
+
+
 
 }
